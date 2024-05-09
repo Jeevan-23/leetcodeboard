7f11897c46266156d45402d89aa2116e0e051e7b
Jeevan M <104501580+Jeevan-23@users.noreply.github.com>
2024-05-09 15:07:22 +0530

    border color white

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,15 @@ import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
-    <div className ="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
-  <div className ="relative px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-   
-        <span style={{ display: 'flex', alignItems: 'center' }}>
-          <Button className="bg-green-500 hover:bg-blue-400 pr-4 mr-7" variant="secondary">Hello</Button>
-          <ModeToggle></ModeToggle>
-        </span>
+    <div className ="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12 ">
+      <div className ="relative px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10 border border-white-500">
+    
+          <span style={{ display: 'flex', alignItems: 'center' }}>
+            <Button className="bg-green-500 hover:bg-blue-400 pr-4 mr-7" variant="secondary">Hello</Button>
+            <ModeToggle></ModeToggle>
+          </span>
 
+      </div>
   </div>
-</div>
   );
 }
