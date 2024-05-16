5d3a35580e52e4e72bcf0eabb255176eee886311
Jeevan M <104501580+Jeevan-23@users.noreply.github.com>
2024-05-16 15:58:57 +0530

    edited the form details

--- app/problems/page.tsx
+++ app/problems/page.tsx
@@ -2,7 +2,13 @@ import CardWithForm from "@/components/card-form"
 
 export default function problems() {
     return (
-     <CardWithForm />
+      <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12 ">
+      <div className="relative px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10 ">
+        <span style={{ display: "flex", alignItems: "center" }}>
+          <CardWithForm />
+        </span>
+      </div>
+    </div>
      
     );
   };
--- components/card-form.tsx
+++ components/card-form.tsx
@@ -11,47 +11,27 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
 
 
 export default function CardWithForm() {
   return (
     <Card className="w-[350px]">
       <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+        <CardTitle>Problems</CardTitle>
+        <CardDescription></CardDescription>
       </CardHeader>
       <CardContent>
         <form>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your project" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Framework</Label>
-              <Select>
-                <SelectTrigger id="framework">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="next">Next.js</SelectItem>
-                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                </SelectContent>
-              </Select>
+              <Label htmlFor="name">Problem id</Label>
+              <Input id="name" placeholder="Enter your Problem id" />
             </div>
           </div>
         </form>
       </CardContent>
       <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
+        <Button>Submit</Button>
       </CardFooter>
     </Card>
   )
