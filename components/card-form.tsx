12513e8045c83a80cee558d6e8b2f2b37d373e38
Jeevan M <104501580+Jeevan-23@users.noreply.github.com>
2024-05-16 23:02:55 +0530

    editing the forms

--- components/card-form.tsx
+++ components/card-form.tsx
@@ -1,40 +1,108 @@
-
-import React from "react"
-import { Button } from "@/components/ui/button"
+import React from "react";
+import { Button } from "@/components/ui/button";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import type { Control, FieldPath } from "react-hook-form";
+import { z } from "zod";
 
+const formSchema = z.object({
+  email: z.string().email(),
+  username: z.string().min(3).max(50),
+  password: z.string().min(8),
+});
 
-export default function CardWithForm() {
+const SignupForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      username: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
-    <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle>Problems</CardTitle>
-        <CardDescription></CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Problem id</Label>
-              <Input id="name" placeholder="Enter your Problem id" />
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button>Submit</Button>
-      </CardFooter>
-    </Card>
-  )
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <SignupFormField
+          name="email"
+          label="Email"
+          placeholder="Email"
+          inputType="email"
+          formControl={form.control}
+        />
+        <SignupFormField
+          name="username"
+          label="Username"
+          placeholder="Username"
+          description="At least 3 characters."
+          formControl={form.control}
+        />
+        <SignupFormField
+          name="password"
+          label="Password"
+          placeholder="Password"
+          description="At least 8 characters."
+          inputType="password"
+          formControl={form.control}
+        />
+        <Button type="submit">Signup</Button>
+      </form>
+    </Form>
+  );
+};
+
+interface SignupFormFieldProps {
+  name: FieldPath<z.infer<typeof formSchema>>;
+  label: string;
+  placeholder: string;
+  description?: string;
+  inputType?: string;
+  formControl: Control<z.infer<typeof formSchema>, any>;
 }
 
+const SignupFormField: React.FC<SignupFormFieldProps> = ({
+  name,
+  label,
+  placeholder,
+  description,
+  inputType,
+  formControl,
+}) => {
+  return (
+    <FormField
+      control={formControl}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              type={inputType || "text"}
+              {...field}
+            />
+          </FormControl>
+          {description && <FormDescription>{description}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
 
+export default SignupForm;
