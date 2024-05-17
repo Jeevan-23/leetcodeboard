e090c22a91bd46bc17002e1f594ebc39790b7d82
Jeevan M <104501580+Jeevan-23@users.noreply.github.com>
2024-05-18 00:42:02 +0530

    kuch not imp changes

--- app/problems/[slug]/page.tsx
+++ app/problems/[slug]/page.tsx
@@ -1,12 +1,20 @@
+"use client"
 
-export default function Page({ params }: { params: { slug: string } }) {
+import { useParams } from "next/navigation"
+
+export default function Page() {
+    
+    const params = useParams()
+   
     return (
         <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12 ">
         <div className="relative px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10 border border-white-100">
           <span style={{ display: "flex", alignItems: "center" }}>
-            Hello
+            Hello, 
+            
           </span>
         </div>
       </div>
+      
     )
   }
