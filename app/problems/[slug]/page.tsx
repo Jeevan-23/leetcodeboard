391ea1301e8d98f81231caff5b218a6bcf53073a
Jeevan M <104501580+Jeevan-23@users.noreply.github.com>
2024-05-18 00:43:28 +0530

    completed the assignment

--- app/problems/[slug]/page.tsx
+++ app/problems/[slug]/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
         <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12 ">
         <div className="relative px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10 border border-white-100">
           <span style={{ display: "flex", alignItems: "center" }}>
-            Hello, 
+            Hello, {params.slug}
             
           </span>
         </div>
