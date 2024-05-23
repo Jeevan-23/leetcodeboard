e3c0fd568de800858d6578edf0672b749e0b4ea6
Jeevan M <104501580+Jeevan-23@users.noreply.github.com>
2024-05-23 19:40:10 +0530

    added excalidraw complete

--- app/problems/[slug]/page.tsx
+++ app/problems/[slug]/page.tsx
@@ -1,20 +1,12 @@
 "use client"
+import { Excalidraw } from "@excalidraw/excalidraw";
 
-import { useParams } from "next/navigation"
-
-export default function Page() {
-    
-    const params = useParams()
-   
-    return (
-        <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12 ">
-        <div className="relative px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10 border border-white-100">
-          <span style={{ display: "flex", alignItems: "center" }}>
-            Hello, {params.slug}
-            
-          </span>
-        </div>
+export default function App() {
+  return (
+    <>
+      <div style={{ height: "500px" }}>
+        <Excalidraw />
       </div>
-      
-    )
-  }
+    </>
+  );
+}
