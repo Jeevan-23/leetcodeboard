053b1b0588e57d87f04f1df9c7577649a1432605
Jeevan M <104501580+Jeevan-23@users.noreply.github.com>
2024-05-25 13:45:26 +0530

    mobile nav not working but working

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -3,6 +3,7 @@ import Link from "next/link"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { MainNav } from "@/components/main-nav"
+import { MobileNav } from "./mobile-nav"
 
 import { ModeToggle } from "@/components/mode-toggle"
 
@@ -11,6 +12,7 @@ export function SiteHeader() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <MainNav />
+        <MobileNav />
 
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
           <nav className="flex items-center">
