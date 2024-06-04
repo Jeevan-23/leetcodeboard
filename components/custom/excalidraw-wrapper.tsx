88b9e33134a4ac856cb7086eff62ca373589d75e
Jeevan M <104501580+Jeevan-23@users.noreply.github.com>
2024-06-04 19:17:04 +0530

    done with welcome screen

--- components/custom/excalidraw-wrapper.tsx
+++ components/custom/excalidraw-wrapper.tsx
@@ -1,12 +1,18 @@
 "use client"
-import {Excalidraw, convertToExcalidrawElements} from "@excalidraw/excalidraw"
+import {Excalidraw, WelcomeScreen} from "@excalidraw/excalidraw"
 
 const ExcalidrawWrapper: React.FC = () => { 
     return (
         <>
-        <div className="flex h-[calc(100vh-3.5rem)] z-50">
-          <Excalidraw />
-        </div>
+       <div className="flex h-[calc(100vh-3.5rem)] z-50">
+      <Excalidraw >
+      <WelcomeScreen>
+          <WelcomeScreen.Hints.ToolbarHint />
+          <WelcomeScreen.Hints.MenuHint />
+          <WelcomeScreen.Hints.HelpHint />
+        </WelcomeScreen>
+      </Excalidraw>
+    </div>
       </>
     );
 }
