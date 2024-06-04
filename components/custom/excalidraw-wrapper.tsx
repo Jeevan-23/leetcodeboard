f57eb38e9ca32f8652a283e81245313ebc27238b
Jeevan M <104501580+Jeevan-23@users.noreply.github.com>
2024-06-05 01:19:11 +0530

    done with persistance

--- components/custom/excalidraw-wrapper.tsx
+++ components/custom/excalidraw-wrapper.tsx
@@ -1,11 +1,35 @@
 "use client"
-import {Excalidraw, WelcomeScreen, MainMenu} from "@excalidraw/excalidraw"
+import {Excalidraw, WelcomeScreen, MainMenu, serializeAsJSON} from "@excalidraw/excalidraw"
+import { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types";
+import { AppState, BinaryFiles } from "@excalidraw/excalidraw/types/types";
+import { useParams } from "next/navigation";
+
+
 
 const ExcalidrawWrapper: React.FC = () => { 
+  const params = useParams()
+  const titleParam = params.slug
+
+  const onChange = (
+    elements: readonly ExcalidrawElement[],
+    appstate: AppState,
+    files: BinaryFiles
+  ) => {
+    const content = serializeAsJSON(elements, appstate, files, "local")
+    localStorage.setItem("excalidraw"+titleParam, content);
+  }
+
+  const retrieveInitialData = () => {
+    const content = localStorage.getItem("excalidraw"+titleParam)
+    if(content != null) {
+      return JSON.parse(content)
+    }
+  }
+
     return (
         <>
        <div className="flex h-[calc(100vh-3.5rem)] z-50">
-      <Excalidraw >
+      <Excalidraw onChange={onChange} initialData={retrieveInitialData()}>
       <WelcomeScreen>
           <WelcomeScreen.Hints.ToolbarHint />
           <WelcomeScreen.Hints.MenuHint />
