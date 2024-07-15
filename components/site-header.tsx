abe21fafa1fd410c94d0100c39cf0da65779c593
Jeevan M <104501580+Jeevan-23@users.noreply.github.com>
2024-07-15 23:12:56 +0530

    made small changes with login logout button

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,9 +1,22 @@
-import { MainNav } from "@/components/main-nav"
-import { MobileNav } from "./mobile-nav"
+"use client";
 
-import { ModeToggle } from "@/components/mode-toggle"
+import { MainNav } from "@/components/main-nav";
+import { MobileNav } from "./mobile-nav";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "./ui/button";
+import { useState } from "react";
 
 export function SiteHeader() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Example state
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <header className="sticky top-0 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center justify-between">
@@ -12,11 +25,16 @@ export function SiteHeader() {
           <MobileNav />
         </div>
         <div className="flex items-center">
+          {isLoggedIn ? (
+            <Button className="mr-2" onClick={handleLogout}>Logout</Button>
+          ) : (
+            <Button className="mr-2" onClick={handleLogin}>Login</Button>
+          )}
           <nav className="flex items-center">
             <ModeToggle />
           </nav>
         </div>
       </div>
     </header>
-  )
+  );
 }
